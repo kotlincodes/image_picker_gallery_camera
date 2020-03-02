c59aa794080574ba0e108bc7365c0a64f577661c
Sayandh <sayandh@acube.co>
2020-03-03 01:53:00 +0530

    Example Added/Package done with simple design

---
+++ example/lib/main.dart
@@ -0,0 +1,91 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker_gallery_camera/image_picker_gallery_camera.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Example Image Picker',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Example Image Picker'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File _image;
+
+  Future getImage(ImgSource source) async {
+    var image = await ImagePickerGC.pickImage(context: context,source: source,cameraIcon: Icon(Icons.add,color: Colors.red,));
+    print(image.toString());
+    setState(() {
+      _image = image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: SingleChildScrollView(
+          child: Column(
+
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 300,
+                child: RaisedButton(
+                  onPressed: ()=>getImage(ImgSource.Gallery),
+                  color: Colors.blue,
+                  child: Text("From Gallery".toUpperCase(),style: TextStyle(color: Colors.white),),
+
+                ),
+              ),
+              Container(
+                width: 300,
+                child: RaisedButton(
+                  onPressed: ()=>getImage(ImgSource.Camera),
+                  color: Colors.deepPurple,
+                  child: Text("From Camera".toUpperCase(),style: TextStyle(color: Colors.white),),
+                ),
+              ),
+              Container(
+                width: 300,
+                child: RaisedButton(
+
+                  onPressed: ()=>getImage(ImgSource.Both),
+                  color: Colors.red,
+                  child: Text("Both".toUpperCase(),style: TextStyle(color: Colors.white),),
+                ),
+              ),
+              _image!=null?Image.file(_image):Container(),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/image_picker_gallery_camera.dart
+++ lib/image_picker_gallery_camera.dart
@@ -1,7 +1,103 @@
 library image_picker_gallery_camera;
+import 'dart:async';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/material.dart';
+
+class ImagePickerGC{
+
+
+  static Future pickImage({
+    @required BuildContext context,
+    @required ImgSource source,
+    double maxWidth,
+    double maxHeight,
+    Icon cameraIcon,
+    Icon galleryIcon,
+    int imageQuality}) async {
+    assert(source != null);
+    assert(imageQuality == null || (imageQuality >= 0 && imageQuality <= 100));
+
+    if (maxWidth != null && maxWidth < 0) {
+      throw ArgumentError.value(maxWidth, 'maxWidth cannot be negative');
+    }
+
+    if (maxHeight != null && maxHeight < 0) {
+      throw ArgumentError.value(maxHeight, 'maxHeight cannot be negative');
+    }
+
+    switch(source){
+
+      case ImgSource.Camera:
+        return await ImagePicker.pickImage(source: ImageSource.camera, maxWidth: maxWidth,maxHeight: maxHeight,imageQuality: imageQuality);
+        break;
+      case ImgSource.Gallery:
+        return await ImagePicker.pickImage(source: ImageSource.gallery, maxWidth: maxWidth,maxHeight: maxHeight,imageQuality: imageQuality);
+        break;
+      case ImgSource.Both:
+         return await showDialog<void>(
+            context: context,
+            barrierDismissible: false, // user must tap button!
+            builder: (BuildContext context) {
+              return  AlertDialog(
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12))
+                ),
+                content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+
+                      InkWell(
+                        onTap: () async {
+                          ImagePicker.pickImage(source: ImageSource.gallery, maxWidth: maxWidth,maxHeight: maxHeight,imageQuality: imageQuality).then((image){
+                            Navigator.pop(context,image);
+
+                          });
+
+                        },
+                        child: Container(
+                          child: ListTile(
+                            title: Text("Gallery"),
+                            leading:galleryIcon!=null?galleryIcon:Icon(Icons.image,color: Colors.deepPurple,)
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 200,
+                        height: 1,
+                        color: Colors.black12,
+                      ),
+                      InkWell(
+                        onTap: () async {
+                          ImagePicker.pickImage(source: ImageSource.camera, maxWidth: maxWidth,maxHeight: maxHeight,imageQuality: imageQuality).then((image){
+                            Navigator.pop(context,image);
+                          });
+                        },
+                        child: Container(
+                          child: ListTile(
+                            title: Text("Camera"),
+                              leading:cameraIcon!=null?cameraIcon:Icon(Icons.camera,color: Colors.deepPurple,)
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  ),
+              );
+
+            },
+          );
+
+
+    }
+
+
+  }
+
+
 }
+
+enum ImgSource { Camera, Gallery,Both }
